db7ee7a135cb112bde72667fcb5630b955e05c00
julius rowe <jmrowe74@gmail.com>
2026-05-04 19:26:59 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- dev.nix
+++ dev.nix
@@ -4,8 +4,22 @@
     pkgs.nodejs_20
     pkgs.busybox
   ];
-  idx.extensions = [
-    "svelte.svelte-vscode"
-    "vue.volar"
-  ];
+  idx.previews = {
+    enable = true;
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "-p"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
 }
